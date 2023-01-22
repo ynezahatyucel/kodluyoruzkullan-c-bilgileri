f235268c0670cc3d090524df0a539683a91855ee
ynezahatyucel <112778295+ynezahatyucel@users.noreply.github.com>
2023-01-22 21:54:09 +0300

    Update kGirisi.java

--- kGirisi.java
+++ kGirisi.java
@@ -18,21 +18,26 @@ public class kGirisi {
 
         }else{
             System.out.println("Yanlış Şifre girdiniz!");
-            System.out.println("Şifrenizi değiştirmek ister misiniz?\nEVET-e\nHAYIR-h");
-            String secim =veri.nextLine();
-            if(secim.equals("e")) {
-                System.out.println("Yeni şifrenizin eski şifreniz ile aynı olmamasına dikkat edin.");
-            }
-            else {
-                System.out.println("Kullanıcı bilgilerini tekrar giriniz");
-            }
-                String npassword= veri.nextLine();
-                if(npassword.equals(paasword) || npassword.equals("patika123")) {
-                    System.out.println("Şifre oluşturulamadı! Lütfen başka bir şifre deneyiniz");
+            System.out.println("Şifrenizi değiştirmek ister misiniz?\nEVET-1\nHAYIR-2");
+            int secim =veri.nextInt();
 
-                }
-                else {
-                    System.out.println("Yeni şifre oluşturuldu.");
+             if(secim==1) {
+            String newUsername;
+            Scanner inp =new Scanner(System.in);
+
+                System.out.println("Yeni şifrenizi giriniz: ");
+                    newUsername = inp.nextLine();
+
+                    if (newUsername.equals(kullanici)){
+                            System.out.println("Yeni şifre eski şifre ile aynı olamaz.");
+                            return;
+                        }else if (!newUsername.equals(kullanici)){
+                            System.out.println("Yeni şifre oluşturuldu. ");
+                        }
+
+                    } else if (secim==2) {
+                        System.out.println("Lütfen bilgilerinizi kontrol ederek tekrar giriş yapmayı deneyiniz.");
+
                 }
 
         }}
